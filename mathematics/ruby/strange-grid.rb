d64c2f9cfc44d0871df95b2d025359f31224ff6c
Robin Drexler <drexler.robin@gmail.com>
2015-07-20 21:19:59 +0200

    Solve grid with formula, I will never ever understand again

--- mathematics/ruby/strange-grid.rb
+++ mathematics/ruby/strange-grid.rb
@@ -1,13 +1,2 @@
 row, column = gets.split(' ').map(&:to_i)
-subtrahend = {
-  true => 1,
-  false => 2
-}
-
-addend = {
-  true => 0,
-  false => 1
-}
-
-row_start_value = (row - subtrahend[row.odd?]) * 5 + addend[row.odd?]
-puts row_start_value + ((column - 1) * 2)
+puts (row - (2-row % 2)) * 5 + (1 - row % 2) + ((column - 1) * 2)
